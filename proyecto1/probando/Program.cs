258242bcd0d575063449f97c3a4ffe15daeb4a91
Jony <jony16344@gmail.com>
2026-01-02 21:15:55 -0600

    Cadena nueva

--- proyecto1/probando/Program.cs
+++ proyecto1/probando/Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, Curso!");
 
-string stripe_api_key = "sk_test_51h342oi3j3io2890";
+string stripe_api_key = "sk_test_51h342oi3j3io289054jk65h4k";
+
+string testingConnection = "Server=localhost;Database=TestDB;User Id=testuser;Password=testpass;";
